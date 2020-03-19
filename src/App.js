7b99de511417fb8f2219b2e403090af61694894b
Safwat Fathi <safwat@iddma.com>
2020-03-19 22:12:17 +0200

    styling sidebar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// import styles
+import styles from "./App.module.css";
+
 // importing components
 import Sidebar from "./components/Sidebar";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className={styles.App}>
         {/* sidemenu nav */}
         <Sidebar />
-        <Switch>
-          {/* main component */}
-          <Route exact path="/">
-            <About />
-          </Route>
-          {/* skills component */}
-          <Route exact path="/skills">
-            <Skills />
-          </Route>
-        </Switch>
+        <div className={styles.content}>
+          <Switch>
+            {/* main component */}
+            <Route exact path="/">
+              <About />
+            </Route>
+            {/* skills component */}
+            <Route exact path="/skills">
+              <Skills />
+            </Route>
+            <Route exact path="/education">
+              <Education />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// import styles
+import styles from "./Sidebar.module.css";
+
 export default function() {
   return (
-    <div>
-      <h1>Sidebar</h1>
-      <ul>
-        <li>
-          <Link to="/">ABOUT</Link>
-        </li>
-        <li>
-          <Link to="/experience">EXPERIENCE</Link>
-        </li>
-        <li>
-          <Link to="/education">EDUCATION</Link>
-        </li>
-        <li>
-          <Link to="/skills">SKILLS</Link>
-        </li>
-        <li>
-          <Link to="/interests">INTERESTS</Link>
-        </li>
-      </ul>
+    <div className={styles.Sidebar}>
+      <nav>
+        <div className="nav_pic">
+          <img src="../../../public/img/personal-pic.jpg" alt="" />
+        </div>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/experience">EXPERIENCE</Link>
+          </li>
+          <li>
+            <Link to="/education">EDUCATION</Link>
+          </li>
+          <li>
+            <Link to="/skills">SKILLS</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
