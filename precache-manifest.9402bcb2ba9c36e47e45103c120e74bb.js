a788b6b537ac48ecbaa92468583398e5703129e2
Safwat Fathi <safwat@iddma.com>
2020-03-19 12:52:23 +0200

    Updates

---
+++ precache-manifest.9402bcb2ba9c36e47e45103c120e74bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c5aacaee9279027670bb404cf17d0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6124b94a36831ea98ac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "233a2a9e1281c4b97fda",
+    "url": "/static/js/2.2cb8c0c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2cb8c0c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6124b94a36831ea98ac",
+    "url": "/static/js/main.2f94ba78.chunk.js"
+  },
+  {
+    "revision": "166cf5f8ec0cbaf35406",
+    "url": "/static/js/runtime-main.d8445b1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
